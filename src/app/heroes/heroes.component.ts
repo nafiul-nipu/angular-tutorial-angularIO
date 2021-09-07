dd2fbde954b75318d521e8585d47e9b4ed4099a1
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2021-09-06 23:02:32 -0500

    route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes', //this components css-element selector like we use in react component name
@@ -11,7 +12,10 @@ export class HeroesComponent implements OnInit {
   heroes : Hero[] = [];
   selectedHero?: Hero ;
   
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService:MessageService
+    ) { }
 
   ngOnInit(): void {
     // lifecycle hook
@@ -21,6 +25,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero:Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes():void {
